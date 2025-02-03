12a334159337b0d699eff019eded1209b725ba11
SvtechAcademy <svtechacademy12m@gmail.com>
2025-02-03 07:41:09 -0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./Pages/Home/Home";
 import TopBar from "./topbar/TopBar";
 
 function App() {
   return (
     <>
     <TopBar/>
+    <Home/>
   
     </>
   );
---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-titles">
+        <span className="headertitlesm">Sv & Tech</span>
+        <span className="headertitlelg" >Blogs</span>
+      </div>
+      <img className ="headerimg"src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTcpxDqtWwSYI4OlzZLdvqlyfmZDeqZpSYkdP6BfK5uhs2A_2UVMx96Z06-koVmBi9BG2xKFcQTKJOFEQbYvq157XiOGUMSe8rhFj0oi2Q" alt="" />
+    </div>
+  )
+}
+
+export default Header
