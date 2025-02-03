b3054913ddbd148537a5643f2b82fe452c359188
SvtechAcademy <svtechacademy12m@gmail.com>
2025-02-03 06:56:05 -0800

    secnd commit

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,24 +3,29 @@ import "./TopBar.css";
 const TopBar = () => {
   return (
     <div className='top'>
-<div className="topleft">
-    <i class="fa-brands fa-square-facebook"></i>
-    <i class="fa-brands fa-twitter"></i>
-    <i class="fa-brands fa-instagram"></i>
-    <i class="fa-brands fa-youtube"></i>
-</div>
-<div className="topcenter">
-    <ul className="toplist">
- <li className="topitemlist">Home</li>
- <li className="topitemlist">About</li>
- <li className="topitemlist">Contact</li>
- <li className="topitemlist">Write</li>
- <li className="topitemlist">Lgout</li>
-    </ul>
-</div>
-<div className="topimg">
-    <img src="../asset/svtech.jpg" alt="svtech" />
-</div>
+      <div className="topleft">
+        <i className="topicon fa-brands fa-square-facebook"></i>
+        <i className="topicon fa-brands fa-twitter"></i>
+        <i className="topicon fa-brands fa-instagram"></i>
+        <i className="topicon fa-brands fa-youtube"></i>
+      </div>
+      <div className="topcenter">
+        <ul className="toplist">
+          <li className="toplistitem">Home</li>
+          <li className="toplistitem">About</li>
+          <li className="toplistitem">Contact</li>
+          <li className="toplistitem">Write</li>
+          <li className="toplistitem">Logout</li>
+        </ul>
+      </div>
+      <div className="topright">
+        <img
+          className="topimg"
+          src="https://avatars.githubusercontent.com/u/193590973?s=48&v=4"
+          alt="svtech"
+        />
+        <i className=" searchicon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 };
